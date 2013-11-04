4daffc1e8ba51c962af3939cb2863cfc410ea859
Satoshi Konno <skonno@cybergarage.org>
2013-11-04 18:23:51 +0900

    - Updated a new sample, upnpdump.

--- samples/upnpdump/src/main/java/UpnpDump.java
+++ samples/upnpdump/src/main/java/UpnpDump.java
@@ -4,13 +4,11 @@
 *
 *	Copyright (C) Satoshi Konno 2002
 *
-*	File : CtrlPoint.java
+*	File : UpnpDump.java
 *
 ******************************************************************/
 
-import java.awt.*;
-
-import javax.swing.*;
+import java.io.*;
 
 import org.cybergarage.util.*;
 import org.cybergarage.upnp.*;
@@ -18,76 +16,26 @@ import org.cybergarage.upnp.ssdp.*;
 import org.cybergarage.upnp.device.*;
 import org.cybergarage.upnp.event.*;
 
-public class CtrlPoint extends ControlPoint implements NotifyListener, EventListener, SearchResponseListener
+public class UpnpDump extends ControlPoint implements NotifyListener, EventListener, SearchResponseListener
 {
-	private final static String TITLE = "CyberLink Sample Control Point";
-	public static int DEFAULT_WIDTH = 640;
-	public final static int DEFAULT_HEIGHT = 480;
-	
-	private JFrame frame;
-	private CtrlPointPane ctrlPointPane;
-	private MenuBar menuBar;
-	
-	public CtrlPoint()
+	////////////////////////////////////////////////
+	//	Constractor
+	////////////////////////////////////////////////
+    
+	public UpnpDump()
 	{
 		addNotifyListener(this);
 		addSearchResponseListener(this);
 		addEventListener(this);
-
-		Debug.on();
-		
-		initFrame();
 	}
-
-	////////////////////////////////////////////////
-	//	Frame
-	////////////////////////////////////////////////
-
-	private void initFrame()
-	{
-		frame = new JFrame(TITLE);
-
-		frame.getContentPane().setLayout(new BorderLayout());
-		
-		menuBar = new MenuBar(this);
-		frame.setJMenuBar(menuBar);
-
-		ctrlPointPane = new CtrlPointPane(this);		
-		getContentPane().add(ctrlPointPane, BorderLayout.CENTER);
-		
-		frame.setSize(new Dimension(DEFAULT_WIDTH,DEFAULT_HEIGHT));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
-
-	public JFrame getFrame()
-	{
-		return frame;
-	}
-	
-	public Container getContentPane()
-	{
-		return getFrame().getContentPane();
-	}
-	
+    
 	////////////////////////////////////////////////
 	//	Graphics
 	////////////////////////////////////////////////
 	
 	public void printConsole(String msg)
 	{
-		ctrlPointPane.printConsole(msg); 
-	}
-
-	public void clearConsole()
-	{
-		ctrlPointPane.clearConsole();
-	}
-
-	public void updateTreeComp()
-	{
-		TreeComp upnpTree = ctrlPointPane.getTreeComp();
-		upnpTree.update(this);
+        System.out.println(msg);
 	}
 	
 	////////////////////////////////////////////////
@@ -113,7 +61,6 @@ public class CtrlPoint extends ControlPoint implements NotifyListener, EventList
 			String nt = packet.getNT();
 			printConsole("ssdp:byebye : uuid = " + usn + ", NT = " + nt); 
 		}
-		updateTreeComp();
 	}
 	
 	public void deviceSearchResponseReceived(SSDPPacket packet)
@@ -122,7 +69,6 @@ public class CtrlPoint extends ControlPoint implements NotifyListener, EventList
 		String st = packet.getST();
 		String url = packet.getLocation();
 		printConsole("device search res : uuid = " + uuid + ", ST = " + st + ", location = " + url); 
-		updateTreeComp();
 	}
 	
 	public void eventNotifyReceived(String uuid, long seq, String name, String value)
@@ -136,16 +82,7 @@ public class CtrlPoint extends ControlPoint implements NotifyListener, EventList
 			
 	public static void main(String args[]) 
 	{
-		//Debug.on();
-		for (int n=0; n<args.length; n++) {
-			String opt = args[n];
-			if (opt.equals("-v") || opt.equals("--verbose")) {
-				Debug.on();
-				Debug.message("Debug.on");			}
-		}
-		
-		CtrlPoint ctrlPoint = new CtrlPoint();
-		ctrlPoint.start();
+		UpnpDump UpnpDump = new UpnpDump();
+		UpnpDump.start();
 	}
-
 }
