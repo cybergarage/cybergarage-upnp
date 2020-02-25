e36617fca3831af572fe355241fbf4694130c70d
Kevin Read <kread@om7sense.com>
2020-02-25 15:34:46 +0100

    Bump version to 2.1.2 Target Java 9 compiler and source level Upload to bintray Drop Xerces and XercesParser (no way to get them to work with Java9) Bump kxml2 to JMPS compatible version Write module-info files

---
+++ std/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cybergarage.upnp.core.upnp.std {
+    requires cybergarage.upnp.core;
+    requires java.prefs;
+    requires java.desktop;
+    requires java.sql;
+    exports org.cybergarage.mediagate.frame.swing;
+    exports org.cybergarage.mediagate.frame;
+}
