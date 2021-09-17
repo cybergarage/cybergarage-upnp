2f3c7fa536749f150ab5043a2dcbe904b04ce0ec
Satoshi Konno <skonno@cybergarage.org>
2021-09-18 03:07:59 +0900

    Update export packages

--- std/src/main/java/module-info.java
+++ std/src/main/java/module-info.java
@@ -3,6 +3,9 @@ module org.cybergarage.upnp.std {
     requires java.prefs;
     requires java.desktop;
     requires java.sql;
-    exports org.cybergarage.mediagate.frame.swings;
-    exports org.cybergarage.mediagate.frame;
+
+    exports org.cybergarage.upnp.std.av.controller;
+    exports org.cybergarage.upnp.std.av.player;
+    exports org.cybergarage.upnp.std.av.renderer;
+    exports org.cybergarage.upnp.std.av.server;
 }
